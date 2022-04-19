0607e95b289d2726a5a42245c84456c41af012ce
Imane <assbaimane@hotmail.com>
2022-04-19 11:41:10 +0200

    Fix: webpack.mix + layouts.app edited so tailwind can compile - IA

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,7 +10,10 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+//Ajouter par bootstrap
 mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css/tailwind.css', [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer')])
     .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
